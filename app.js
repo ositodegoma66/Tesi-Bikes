be4fe71b0547e5e52acb9d844f53cba84c23fdaf
Daniel Cruz <xmasterdaan99@gmail.com>
2026-05-17 21:46:22 -0600

    Agregar lista de bicicletas con estados y colores

--- app.js
+++ app.js
@@ -13,3 +13,29 @@ function mostrarSeccion(nombre) {
     document.getElementById('seccion-' + nombre).classList.add('activa');
     document.getElementById('tab-' + nombre).classList.add('activa');
 }
+
+const flota = [
+    { id: 'TESI-01', estado: 'libre', bateria: 98, ubicacion: 'Entrada principal' },
+    { id: 'TESI-02', estado: 'en_uso', bateria: 72, ubicacion: 'En transito' },
+    { id: 'TESI-03', estado: 'libre', bateria: 85, ubicacion: 'Biblioteca' },
+    { id: 'TESI-04', estado: 'mantenimiento', bateria: 20, ubicacion: 'Area Deportiva' },
+    { id: 'TESI-05', estado: 'libre', bateria: 100, ubicacion: 'Laboratorio Sistemas' },
+];
+
+function mostrarBicicletas() {
+    let contenedor = document.getElementById('lista-bicicletas');
+    contenedor.innerHTML = '';
+
+    flota.forEach(function(bici) {
+    contenedor.innerHTML += `
+    <div class="tarjeta-bici">
+        <span class="bici-id">${bici.id}</span>
+        <span class="bici-ubicacion">${bici.ubicacion}</span>
+        <span class="bici-bateria">🔋 ${bici.bateria}%</span>
+        <span class="estado-${bici.estado}">${bici.estado}</span>
+    </div>
+`;    
+    });
+}
+
+mostrarBicicletas();
